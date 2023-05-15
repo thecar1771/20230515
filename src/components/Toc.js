02de46a1c270ecc7df8a37da77ae527493a09b27
thecar1771 <48800572+thecar1771@users.noreply.github.com>
2023-05-15 12:47:52 +0900

    Initial commit

---
+++ src/components/Toc.js
@@ -0,0 +1,45 @@
+import React, { Component } from "react";
+
+class Toc extends Component {
+  shouldComponentUpdate(newProps, newState) {
+    console.log(newProps.data, this.props.data);
+    // list data push로 넣을 경우 원본이 변하기 때문에 비교가 불가함
+    if (this.props.data === newProps.data) {
+      return false;
+    }
+
+    return true;
+  }
+
+  render() {
+    var lists = [];
+    var data = this.props.data;
+    var i = 0;
+    while (i < data.length) {
+      lists.push(
+        <li key={data[i].id}>
+          <a
+            href={"/content/" + data[i].id}
+            data-id={data[i].id}
+            onClick={function (id, e) {
+              e.preventDefault();
+              //this.props.onChangePage(e.target.dataset.id);
+              this.props.onChangePage(id);
+            }.bind(this, data[i].id)}
+          >
+            {data[i].title}
+          </a>
+        </li>
+      );
+      ++i;
+    }
+
+    return (
+      <nav>
+        <ul>{lists}</ul>
+      </nav>
+    );
+  }
+}
+
+export default Toc;
